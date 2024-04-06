46ee873b0223d3a94c23e4542c6934e04f80cd47
Javier Monardo <javorengero2000@yahoo.com.ar>
2024-04-05 21:47:03 -0300

    CounterPage component created for it to render the counter. As it is set as a client component, it's not allowed it has metadata. If I turn this component as a server component by deleting the command use client, then I can't use useState. Therefore, in the next commit I'll fix this issue by turning this component as a server component and creating a new client component only for the buttons which are the real dynamic elements.

--- src/app/dashboard/counter/page.tsx
+++ src/app/dashboard/counter/page.tsx
@@ -1,7 +1,37 @@
+"use client";
+import { Metadata } from "next";
+import { useState } from "react";
+
+export const metadata: Metadata = {
+  title: "Counter Page",
+  description: "Un simple contador",
+};
+
 export default function CounterPage() {
+  const [counter, setCounter] = useState<number>(0);
+
   return (
-    <div>
-      <h1>Counter Page</h1>
+    <div className="flex flex-col justify-center items-center h-full">
+      <span>Productos en el carrito</span>
+      <span className="text-9xl">{counter}</span>
+      <div className="flex items-center justify-center">
+        <button
+          className="p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2"
+          onClick={() => {
+            setCounter(counter + 1);
+          }}
+        >
+          +1
+        </button>
+        <button
+          className="p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2"
+          onClick={() => {
+            if (counter > 0) setCounter(counter - 1);
+          }}
+        >
+          -1
+        </button>
+      </div>
     </div>
   );
 }
