8d47e1099959c5fa1c55320314eb4e078f4a7f61
Javier Monardo <javorengero2000@yahoo.com.ar>
2024-04-09 14:16:46 -0300

    PokemonsPage component created - New item for Sidebar component that renders the PokemonsPage component

--- src/app/dashboard/pokemons/page.tsx
+++ src/app/dashboard/pokemons/page.tsx
@@ -1,7 +1,26 @@
-export default function PokemonsPage() {
-  return (
-    <div>
-      <h1>Hello Pokemons Page</h1>
-    </div>
-  );
+import { ReactNode } from "react";
+
+const getPokemons = async (limit = 20, offset = 0): Promise<void> => {
+  try {
+    const data = await fetch(
+      `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`
+    );
+    const dataToJson = data.json();
+    return Promise.resolve(dataToJson);
+  } catch (error) {
+    console.error(error);
+    throw new Error("Error al conectar a la API.");
+  }
+};
+
+export default async function PokemonsPage(): Promise<ReactNode> {
+  try {
+    const pokemons = await getPokemons(151);
+    return <div>{JSON.stringify(pokemons)}</div>;
+  } catch (err) {
+    if (err instanceof Error) {
+      return <div>{err.message}</div>;
+    }
+    return "Error";
+  }
 }
